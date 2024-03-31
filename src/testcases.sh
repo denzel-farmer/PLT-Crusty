82eac275a8ab0eb64b005ac46c691aa64677495f
ptong2556 <pt2556@columbia.edu>
2024-03-31 13:19:28 -0400

    Basic shell-scripting framework to test parser + scanner

---
+++ src/testcases.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+check_test() {
+    local case="$1"
+    local expected="$2"
+
+    # also redirect stderr so that error messages from invalid 
+    # programs are not shown 
+    echo "$case" | ./test > /dev/null 2>&1
+    exit=$? 
+    
+    if [ $exit -ne 0 ] && [ $expected = "invalid" ]; then 
+        echo "Correct Output for Test Case" 
+    elif [ $exit -eq 0 ] && [ $expected = "valid" ]; then 
+        echo "Correct Output for Test Case" 
+    else 
+        echo "Incorect Output for Test Case"
+    fi
+}
+
+testcases=("int x;" "int y")
+expected=("valid" "invalid")
+
+for num in "${!testcases[@]}"; do 
+    check_test "${testcases[$num]}" "${expected[$num]}"
+done
+
