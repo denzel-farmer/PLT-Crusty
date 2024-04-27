24c6a1219f7301e75090aff8752ec2ba72816aa7
ptong2556 <pt2556@columbia.edu>
2024-04-27 14:15:18 -0400

    Operation semantic checking, this is going to take so long why didn't we start earlier lol

--- src/sast.mli
+++ src/sast.mli
@@ -1,12 +1,39 @@
 open Ast
 
-type sexpr = typ * expr_detail 
-and expr_detail =   
+type sexpr = typ * expr 
+and expr =   
       SId of string
     | SLiteral of literal
     | SOperation of operation
     | SAssignment of assignment
     | SCall of string * sexpr list
+
+(* Operation Types *)
+and operation =
+    | SArithOp of sexpr * binArithOp * sexpr
+    | SUnArithOp of unArithOp * sexpr
+    | SCompOp of sexpr * compOp * sexpr
+    | SLogOp of sexpr * binLogOp * sexpr
+    | SUnLogOp of unLogOp * sexpr
+    | SAccessOp of sexpr * accessOp * string
+    | SDeref of sexpr
+    | SBorrow of sexpr
+  
+(* Assignment Types *)
+and assignment =
+    | SAssign of string * sexpr
+    | SStructAssign of string * string * sexpr
+    | SRefStructAssign of string * string * sexpr
+    | SStructExplode of string list * sexpr
+
+and literal =
+  | SIntLit of int
+  | SBoolLit of bool
+  | SCharLit of char
+  | SFloatLit of float
+  | SStructLit of sexpr list
+  | SStringLit of string
+  | SArrayLit of sexpr list
 
 (* Statement Types *)
 type sstmt =
@@ -14,8 +41,8 @@ type sstmt =
   | SExpr of sexpr
   | SIf of sexpr * sstmt * sstmt
   | SWhile of sexpr * sstmt
-  (* | Break
-  | Continue *)
+  | SBreak
+  | SContinue
 
 (* Function Definition Type *)
 type sfunc_def =
@@ -34,24 +61,9 @@ type sstruct_def =
 }
 
 (* Program Type *)
-type sprogram =
+type program =
   { sglobals : const_qualified_var_decl list
   ; sstructs : sstruct_def list
   ; sfuncs : sfunc_def list
   }
-
-
-(* Declaration Types *)
-type var_decl = linear_qual * typ * string
-
-(* type array_decl = string * int
-
-type const_array_decl = 
-  | Arr of var_decl *)
-
-type const_qualified_var_decl =
-  | Const of var_decl
-  | Var of var_decl
-
-type arg_decl = ref_qual * var_decl
 
--- src/semant.ml
+++ src/semant.ml
@@ -2,6 +2,7 @@
 
 open Ast
 open Sast
+open Astprint
 
 module StringMap = Map.Make(String)
 
@@ -51,35 +52,187 @@ let check (globals, structs, functions) =
   in
   let _ = find_func "main" in (* Ensure "main" is defined *)
 
+  (* TODO: Add checking for structs to make sure no duplicate variables *)
+  let check_struct struct = 
+    (* struct.name *)
+    (* struct.fields *)
+
+  in 
+
   let check_fun func = 
-      check_binds "arg" func.args;
-      check_binds "local" func.locals;
+    check_binds "arg" func.args;
+    check_binds "local" func.locals;
 
-      let add_symbol m (ty, name) = StringMap.add name ty m 
-      in 
-      let symbols = List.fold_left add_symbol StringMap.empty (globals @ func.args @ func.locals)
-      in 
-      (* Return a variable from our local symbol table *)
-      let type_of_identifier s =
-        try StringMap.find s symbols
-        with Not_found -> raise (Failure ("undeclared identifier " ^ s))
-      in 
-      let rec check_stmt_list l = 
-      match l with 
-      [] -> [] 
-      | s :: sl -> check_stmt s :: check_stmt_list sl
-      and check_stmt s = 
-        match s with 
-        Block sl -> SBlock(check_stmt_list sl)
+    (* Raise an exception if the given rvalue type cannot be assigned to
+    the given lvalue type *)
+    let check_assign lvaluet rvaluet err =
+      if lvaluet = rvaluet then lvaluet else raise (Failure err)
+    in
 
-      in 
+    let add_symbol m (ty, name) = StringMap.add name ty m 
+    in 
+    let symbols = List.fold_left add_symbol StringMap.empty (globals @ func.args @ func.locals)
+    in 
 
-      { srtyp=func.rtype
-      ; sfname=func.fname
-      ; sargs=func.args
-      ; slocals=func.locals
-      ; sbody=check_stmt_list func.body
-      ; sreturn=func.return
-      }
+    (* Return a variable from our local symbol table *)
+    let type_of_identifier s =
+      try StringMap.find s symbols
+      with Not_found -> raise (Failure ("undeclared identifier " ^ s))
+    in 
+
+    (* Return a semantically-checked expression, i.e., with a type *)
+    let rec check_expr = function
+      Id var -> (type_of_identifier var, SId var)
+      | Literal l -> check_literal l 
+      | Assign l -> check_all_assignment l
+      | Operation l -> check_operation l
+      | Call (fname, args) as call -> 
+        let fd = find_func fname in
+        let param_length = List.length fd.args in
+        if List.length args != param_length then
+          raise (Failure ("expecting " ^ string_of_int param_length ^
+                          " arguments in " ^ string_of_expr call))
+        else let check_call (ft, _) e =
+               let (et, e') = check_expr e in
+               let err = "illegal argument found " ^ string_of_typ et ^
+                         " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e
+               in (check_assign ft et err, e')
+          in
+          let args' = List.map2 check_call fd.formals args
+          in (fd.rtyp, SCall(fname, args'))
+    in 
+    (* Check literals*)
+    let check_literal = function 
+        IntLit l -> (Int, SIntLit l)
+      | BoolLit l -> (Bool, SBoolLit l)
+      | CharLit l -> (Char, SCharLit l)
+      | FloatLit l -> (Float, SFloatLit l)
+      (* | StructLit l -> (Struct, ) *)
+      | StringLit l -> (String, SStringLit l)
+      (* | ArrayLit l -> () *)
+    in 
+    (* Check assignments*) 
+    let check_all_assignment = function 
+        Assign (var, e) -> ()
+      | StructAssign (var1, var2, e) -> ()
+      | RefStructAssign (var1, var2, e) -> ()
+      (* | StructExplode (var, e) *)
+    in 
+    (* Check operations *)
+    let check_operation = function 
+        ArithOp (e1, op, e2) -> 
+        let (t1, e1') = check_expr e1
+        and (t2, e2') = check_expr e2 in
+        let err = "illegal binary operator " ^
+                  string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                  string_of_typ t2 ^ " in " ^ string_of_expr e
+        in
+        (* All binary operators require operands of the same type*)
+        if t1 = t2 then
+          (* Determine expression type based on operator and operand types *)
+          let t = match op with
+              Add | Sub | Mul | Div when t1 = Int -> Int
+              Add | Sub | Mul | Div when t1 = Float -> Float
+            | _ -> raise (Failure err)
+          in
+          (t, SArithop((t1, e1'), op, (t2, e2')))
+        else raise (Failure err)
+      | UnArithOp (op, e) -> 
+        let (t, e) = check_expr e1 in 
+        let err = "illegal unary operation " ^ string_of_typ t 
+        in  
+        let t = match op with 
+           
+        in 
+        (t, SUnArithop(op, (t, e))
+      | CompOp (e1, op, e2) -> 
+        let (t1, e1') = check_expr e1
+        and (t2, e2') = check_expr e2 in
+        let err = "illegal compare operator " ^
+                  string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                  string_of_typ t2 ^ " in " ^ string_of_expr e
+        in
+        (* All compare operators require operands of the same type*)
+        if t1 = t2 then
+          let t = match op with
+              Eq | Neq -> Bool 
+            | Lt | Gt | Leq | Geq when t1 = Int -> Bool 
+            | Lt | Gt | Leq | Geq when t1 = Float -> Bool 
+            | _ -> raise (Failure err)
+          in
+          (t, SCompOp((t1, e1'), op, (t2, e2')))
+        else raise (Failure err)
+      | LogOp (e1, op, e2) -> 
+        let (t1, e1') = check_expr e1
+        and (t2, e2') = check_expr e2 in
+        let err = "illegal binary logical operator " ^
+                  string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                  string_of_typ t2 ^ " in " ^ string_of_expr e
+        in
+        (* Both must be bools *)
+        if t1 = t2 then
+          let t = match op with
+              And | Or when t1 = Bool -> Bool
+            | _ -> raise (Failure err)
+          in
+          (t, SLogOp((t1, e1'), op, (t2, e2')))
+        else raise (Failure err)
+      | UnLogOp (op, e) -> 
+        let (t, e') = check_expr e in 
+        let err = "illegal unary logical operator " ^
+                  string_of_typ t
+        in 
+        if t != Bool -> raise (Failure err)
+        in
+        (t, SUnLogOp(op, (t, e')))
+      | AccessOp (e, op, var) -> 
+        let (t, e') = check_expr e in
+        let err = "illegal access operator " ^
+        string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+        string_of_typ t2 ^ " in " ^ string_of_expr e
+in 
+        if t != Struct 
+
+      | Deref (e) -> () 
+      | Borrow (e) -> ()
+    in 
+
+      
+    let check_bool_expr e =
+      let (t, e') = check_expr e in
+      match t with
+      | Bool -> (t, e')
+      |  _ -> raise (Failure ("expected Boolean expression in " ^ string_of_expr e))
+    in
+
+    let rec check_stmt_list l = 
+    match l with 
+    [] -> [] 
+    | s :: sl -> check_stmt s :: check_stmt_list sl
+    and check_stmt s = 
+      match s with 
+      Block sl -> SBlock (check_stmt_list sl)
+      | Expr e -> SExpr (check_expr e)
+      | If(e, st1, st2) ->
+        SIf(check_bool_expr e, check_stmt st1, check_stmt st2)
+      | While(e, st) ->
+        SWhile(check_bool_expr e, check_stmt st)
+      | Break -> SBreak
+      | Continue -> SContinue
+      | Return e ->
+        let (t, e') = check_expr e in
+        if t = func.rtyp then SReturn (t, e')
+        else raise (
+            Failure ("return gives " ^ string_of_typ t ^ " expected " ^
+                      string_of_typ func.rtyp ^ " in " ^ string_of_expr e)) 
+    in 
+
+    { srtyp=func.rtype
+    ; sfname=func.fname
+    ; sargs=func.args
+    ; slocals=func.locals
+    ; sbody=check_stmt_list func.body
+    ; sreturn=func.return
+    }
 in 
 (globals, structs, List.map check_fun functions)
