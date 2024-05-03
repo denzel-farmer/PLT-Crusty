2ce26d7e6526faa13c717b5f83122c5c1d43414b
Denzel Farmer <denzelgfarmer@gmail.com>
2024-05-03 02:18:45 -0400

    Updates to sast

--- src/sast.mli
+++ src/sast.mli
@@ -1,34 +1,33 @@
 open Ast
 
-(* type styp = 
-    STyp of linear_qual * typ *)
-type var_type = linear_qual * typ 
+(* typ includes reference and linear qualifier if primative *)
+type sexpr = typ * sx
 
-type sexpr = var_type * sx
-and sx =   
-      SId of string
-    | SLiteral of literal
-    | SOperation of soperation
-    | SAssignment of sassignment
-    | SCall of string * sexpr list
+and sx =
+  | SId of string
+  | SLiteral of literal
+  | SOperation of soperation
+  | SAssignment of sassignment
+  | SCall of string * sexpr list
 
 (* Operation Types *)
 and soperation =
-    | SArithOp of sexpr * binArithOp * sexpr
-    | SUnArithOp of unArithOp * sexpr
-    | SCompOp of sexpr * compOp * sexpr
-    | SLogOp of sexpr * binLogOp * sexpr
-    | SUnLogOp of unLogOp * sexpr
-    | SAccessOp of sexpr * accessOp * string
-    | SDeref of sexpr
-    | SBorrow of sexpr
-  
+  | SArithOp of sexpr * binArithOp * sexpr
+  | SUnArithOp of unArithOp * sexpr
+  | SCompOp of sexpr * compOp * sexpr
+  | SLogOp of sexpr * binLogOp * sexpr
+  | SUnLogOp of unLogOp * sexpr
+  | SAccessOp of sexpr * accessOp * string
+  | SDeref of sexpr
+  | SBorrow of sexpr
+  | SIndex of string * sexpr
+
 (* Assignment Types *)
 and sassignment =
-    | SAssign of string * sexpr
-    | SStructAssign of string * string * sexpr
-    | SRefStructAssign of string * string * sexpr
-    | SStructExplode of string list * sexpr
+  | SAssign of string * sexpr
+  | SStructAssign of string * string * sexpr
+  | SRefStructAssign of string * string * sexpr
+  | SStructExplode of string list * sexpr
 
 and sliteral =
   | SIntLit of int
@@ -48,26 +47,32 @@ type sstmt =
   | SBreak
   | SContinue
 
+type sreturn_stmt =
+  | SReturn of sexpr
+  | SVoidReturn
+
+(* Argument Declaration Type *)
+
 (* Function Definition Type *)
 type sfunc_def =
-  { srtyp : typ
+  { srtyp : ret_typ
   ; sfname : string
-  ; sargs : arg_decl list
-  ; slocals : const_qualified_var_decl list
+  ; sargs : var_decl list
+  ; slocals : var_decl list
   ; sbody : sstmt list
-  ; sreturn: return_stmt
+  ; sreturn : sreturn_stmt
   }
 
 (* Struct Definition Type *)
 type sstruct_def =
-{ sname : string
-; sfields : const_qualified_var_decl list
-}
+  { lin_qual : lin_qual
+  ; sname : string
+  ; sfields : var_decl list
+  }
 
 (* Program Type *)
 type program =
-  { sglobals : const_qualified_var_decl list
+  { sglobals : var_decl list
   ; sstructs : sstruct_def list
   ; sfuncs : sfunc_def list
   }
-
