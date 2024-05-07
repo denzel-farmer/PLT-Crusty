cfd18b368f257c11eaaa4e49bcc08eda44b40748
Denzel Farmer <denzelgfarmer@gmail.com>
2024-05-07 19:07:46 -0400

    Add linear check to checksemant.ml runner

--- src/checksemant.ml
+++ src/checksemant.ml
@@ -2,5 +2,7 @@ let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Crustyparse.program Scanner.token lexbuf in
   let sprogram = Semant2.check program in
+  let linear_result = Linear.check sprogram in
   print_endline (Astprint.string_of_program program);
-  print_endline (Sastprint.string_of_sprogram sprogram)
+  print_endline (Sastprint.string_of_sprogram sprogram);
+  print_endline (string_of_bool (Result.is_error linear_result));
