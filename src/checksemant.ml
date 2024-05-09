2cd78129a90ddc463f787e4bba575a5ae3330fa1
Denzel Farmer <denzelgfarmer@gmail.com>
2024-05-09 00:43:30 -0400

    Update checksemant.ml for semant2 -> semant

--- src/checksemant.ml
+++ src/checksemant.ml
@@ -1,7 +1,7 @@
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Crustyparse.program Scanner.token lexbuf in
-  let sprogram = Semant2.check program in
+  let sprogram = Semant.check program in
   let linear_result = Linear.check sprogram in
   print_endline (Astprint.string_of_program program);
   print_endline (Sastprint.string_of_sprogram sprogram);
