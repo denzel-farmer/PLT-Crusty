a01a554e41f0e1ecfb79c28ea57c0c900d9572f1
Peter Ma <peterma1999@gmail.com>
2024-05-11 12:47:57 -0400

    (irgen) cleanup part 1

--- src/irgen.ml
+++ src/irgen.ml
@@ -5,157 +5,126 @@ open Sast
 module StringMap = Map.Make(String)
 
 (* translate : Sast.program -> Llvm.module *)
-let translate (globals, structs, functions) =
+let translate ((globals : A.var_decl list), (structs : A.struct_def list), (functions : sfunc_def list)) =
   let context    = L.global_context () in
-  let the_module = L.create_module context "Crusty" in
+  let crusty_module = L.create_module context "Crusty" in
 
+  (* LLVM integer types *)
   let i64_t      = L.i64_type       context
   and i32_t      = L.i32_type       context
   and i8_t       = L.i8_type        context
   and i1_t       = L.i1_type        context 
-  (* and iString_t  = L.pointer_type (L.i8_type context) *)
   in
   
-  let all_structs : (A.struct_def) StringMap.t =
-    let struct_decl m (sdecl: A.struct_def) =
-      let name = sdecl.sname in
-      StringMap.add name (sdecl) m in
-    List.fold_left struct_decl StringMap.empty structs
+  (* Map of struct name to struct definition *)
+  let struct_decls : (A.struct_def) StringMap.t =
+    let add_struct_def m (sdef: A.struct_def) =
+      let name = sdef.sname in
+      StringMap.add name (sdef) m in
+    List.fold_left add_struct_def StringMap.empty structs
   in
 
-  (* function to find out the offset of a given field in a struct *)
-  let find_field_num all_fields field = 
-    let rec find_field fields field num = 
-      match fields with 
-      | [] -> -1 
-      | hd :: tl ->
-        let field' = match hd with 
-          | (_, f) -> f
-        in
-        if field' = field then num else find_field tl field num + 1 
-    in
-    find_field all_fields field 0 
-  in
-
-  (* let rec struct_fields s = 
-    let struct_decl = StringMap.find s struct_decls in
-    let field_types = List.map (fun (t, _) -> ltype_of_typ t) struct_decl.fields in
-    Array.of_list field_types
-  in *)
-
-  let rec ltype_of_typ = function
-    A.Prim(_, Int) -> i32_t
-  | A.Prim(_, Char) -> i8_t
-  | A.Prim(_, Bool) -> i1_t
-  | A.Prim(_, Float) -> L.float_type context
-  | A.Prim(_, String) -> L.pointer_type i8_t
-  | A.Struct(s) ->
-    let struct_decl = StringMap.find s all_structs in
-    let field_types = List.map (fun (t, _) -> ltype_of_typ t) struct_decl.fields in
-    L.struct_type context (Array.of_list field_types)
-  | A.Arr(t, n) -> L.array_type (ltype_of_typ t) n 
-  | A.Ref(t) -> L.pointer_type (ltype_of_typ t)
-  (* | A.Ref(t) ->  *) (* TODO *)
+  (* Convert AST type to LLVM type *)
+  let rec ltype_of_typ : (A.typ -> L.lltype) = function
+      A.Prim(_, Int) -> i32_t
+    | A.Prim(_, Char) -> i8_t
+    | A.Prim(_, Bool) -> i1_t
+    | A.Prim(_, Float) -> L.float_type context
+    | A.Prim(_, String) -> L.pointer_type i8_t
+    | A.Struct(s) ->
+      let struct_def = StringMap.find s struct_decls in
+      let field_types = List.map (fun (t, _) -> ltype_of_typ t) struct_def.fields in
+      L.struct_type context (Array.of_list field_types)
+    | A.Ref(t) -> L.pointer_type (ltype_of_typ t)
+    (* | A.Arr(t, n) -> L.array_type (ltype_of_typ t) n  *)
   in 
 
-  let rec ltype_of_rtyp = function 
-    A.Nonvoid(t) -> ltype_of_typ t
+  let rec ltype_of_rtyp : (A.ret_typ -> L.lltype) = function 
+      A.Nonvoid(t) -> ltype_of_typ t
     | A.Void -> L.void_type context
   in
 
-  let struct_decls : (L.lltype) StringMap.t =
-    let struct_decl m (sdecl: A.struct_def) =
-      let name = sdecl.sname in
-      let field_types = List.map (fun (t, _) -> ltype_of_typ t) sdecl.fields in
-      StringMap.add name (L.struct_type context (Array.of_list field_types)) m in
-    List.fold_left struct_decl StringMap.empty structs
-  
-  in
-  (* Define each function (arguments and return type) so we can
-     call it even before we've created its body *)
-  let function_decls : (L.llvalue * sfunc_def) StringMap.t =
-    let function_decl m fdecl =
-      let name = fdecl.sfname
-      and formal_types =
-        Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) fdecl.sargs)
-      in 
-      let ftype = L.function_type (ltype_of_rtyp fdecl.srtyp) formal_types in
-      StringMap.add name (L.define_function name ftype the_module, fdecl) m 
-    in
-    List.fold_left function_decl StringMap.empty functions
-  in
-    
+  (* Build LLVM literal from Crusty literal *)
   let build_literal lit builder =
     match lit with
     | SIntLit l -> L.const_int i32_t l
     | SBoolLit l -> L.const_int i1_t (if l then 1 else 0)
     | SCharLit l -> L.const_int i8_t (Char.code l)
     | SFloatLit l -> L.const_float (L.float_type context) l
-    | SStringLit s -> 
-      L.build_global_stringptr s "globalstring" builder in
+    | SStringLit s -> L.build_global_stringptr s "globalstring" builder in
 
-      (* let str = L.build_global_stringptr s "globalstring" builder in
-      L.const_bitcast str (L.pointer_type i8_t) *)
+  (* Map of function name to (LLVM type, function definition) *)
+  let function_decls : (L.llvalue * sfunc_def) StringMap.t =
+    let add_function_decl m fdecl =
+      let name = fdecl.sfname
+      and args_types =
+        Array.of_list (List.map (fun (t,_) -> ltype_of_typ t) fdecl.sargs)
+      in 
+      let ftype = L.function_type (ltype_of_rtyp fdecl.srtyp) args_types in
+      StringMap.add name (L.define_function name ftype crusty_module, fdecl) m 
+    in
+    List.fold_left add_function_decl StringMap.empty functions
+  in
 
-  (* Create a map of global variables after creating each *)
+  (* Scoped symbol table *)
+  let symbols = ref [] in
+
+  let enter_scope () = symbols := StringMap.empty :: !symbols in
+  
+  let exit_scope () = match !symbols with
+    | [] -> raise (Failure "No scope to exit")
+    | _ :: tl -> symbols := tl
+  in
+
+  (* Add globals to symbol table *)
   let global_vars : (L.llvalue * A.typ) StringMap.t =
     let global_var m (t, n) =
       let init = match t with
         A.Prim (_, _) -> L.const_int (ltype_of_typ t) 0
       (* | A.Arr(_) -> ltype_of_typ t *)
-      in StringMap.add n (L.define_global n init the_module, t) m 
+      in StringMap.add n (L.define_global n init crusty_module, t) m 
     in
     List.fold_left global_var StringMap.empty globals
   in
   
-  (* let struct_defs : L.llvalue StringMap.t =
-    let struct_def m l =
-      let init = ltype_of_typ l
-      in StringMap.add l.sname (L.define_global n init the_module) m 
-    in
-    List.fold_left struct_def StringMap.empty structs 
-  in *)
-
-  (* Define the printf function *)
-
-  (* let printf_t : L.lltype =
-    L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-  let printf_func : L.llvalue =
-    L.declare_function "printf" printf_t the_module in *)
-
-  let scopes = ref [] in
-
-  let enter_scope () = scopes := StringMap.empty :: !scopes in
-  
-  let exit_scope () = match !scopes with
-    | [] -> raise (Failure "No scope to exit")
-    | _ :: tl -> scopes := tl
-  in
-  
-  (* let add_local_types var_name typ map = 
-    StringMap.add var_name typ map
-  in 
-  let = add_ *)
-
+  (* Helper function to add locals to symbol table *)
   let add_local builder var_name var_type lltype =
-    let current_map = List.hd !scopes in
+    let current_map = List.hd !symbols in
     let alloca = L.build_alloca lltype var_name builder in
-    scopes := (StringMap.add var_name (alloca, var_type) current_map) :: (List.tl !scopes);
+    symbols := (StringMap.add var_name (alloca, var_type) current_map) :: (List.tl !symbols);
     alloca
   in
   
   let find_local var_name =
-    let rec search (scopes) = match scopes with
+    let rec search (symbols) = match symbols with
       | [] -> raise (Failure ("Unbound variable " ^ var_name))
       | h :: t -> 
         try StringMap.find var_name h with Not_found -> search t
-    in search !scopes   
+    in search !symbols   
+  in
+
+  (* Return the value for a variable or formal argument.
+      Check local names first, then global names *)
+  let lookup n = try find_local n
+    with Failure _ -> StringMap.find n global_vars
+  in
+
+  (* function to find the offset of a given field in a struct *)
+  let get_field_idx (all_fields : A.var_decl list) (field : string) = 
+    let rec find_field fields field num = 
+      match fields with 
+      | [] -> raise (Failure ("Struct field not found: " ^ field)) 
+      | hd :: tl ->
+        let (_, field') = hd in
+        if field' = field then num else find_field tl field (num + 1)
+    in
+    find_field all_fields field 0 
   in
 
   let printf_t : L.lltype =
     L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func : L.llvalue =
-    L.declare_function "printf" printf_t the_module in
+    L.declare_function "printf" printf_t crusty_module in
 
   let build_function_body fdecl =
     let (function_addr, _) = StringMap.find fdecl.sfname function_decls in
@@ -185,12 +154,6 @@ let translate (globals, structs, functions) =
     (* List.iter (fun (ty, n) ->
       ignore (add_local builder n (ltype_of_typ ty))
     ) fdecl.slocals; *)
-
-    (* Return the value for a variable or formal argument.
-       Check local names first, then global names *)
-    let lookup n = try find_local n
-      with Failure _ -> StringMap.find n global_vars
-    in
     let rec build_expr builder ((_, e) : sexpr) = match e with
         | SId s -> let addr, _ = lookup s in
           L.build_load (addr) s builder
@@ -214,9 +177,9 @@ let translate (globals, structs, functions) =
             let e' = build_expr builder e in
             let (struct', st) = lookup s1 in
             let s = match st with 
-              A.Struct(s) -> StringMap.find s all_structs
+              A.Struct(s) -> StringMap.find s struct_decls
             in 
-            let index = find_field_num s.fields s2 in
+            let index = get_field_idx s.fields s2 in
             (* pointer to the field index at this struct *)
             let field' = L.build_struct_gep struct' index "tmp" builder in
             (* let field'' = L.build_load field' "tmp" builder in *)
@@ -226,9 +189,9 @@ let translate (globals, structs, functions) =
             let e' = build_expr builder e in
             let (s, st) = lookup s1 in
             let t' = match st with 
-              A.Ref(Struct(s)) -> StringMap.find s all_structs
+              A.Ref(Struct(s)) -> StringMap.find s struct_decls
             in 
-            let index = find_field_num t'.fields s2 in
+            let index = get_field_idx t'.fields s2 in
             let struct' = L.build_load s "tmp" builder in
             let field' = L.build_struct_gep struct' index "tmp" builder in
             L.build_store e' field' builder
@@ -238,7 +201,7 @@ let translate (globals, structs, functions) =
             | SId struct_name -> 
                 let (struct_addr, struct_typ) = lookup struct_name in
                 let struct_decl = match struct_typ with
-                  | A.Struct(sname) -> StringMap.find sname all_structs
+                  | A.Struct(sname) -> StringMap.find sname struct_decls
                 in
                 List.iteri (fun idx var_name ->
                     let field_ptr = L.build_struct_gep struct_addr idx "field_ptr" builder in
@@ -302,17 +265,17 @@ let translate (globals, structs, functions) =
                   let (struct', st) = lookup s1 in
                   let struct_val = L.build_load struct' "tmp" builder in
                   let s = match st with 
-                    A.Ref(Struct(s)) -> StringMap.find s all_structs
+                    A.Ref(Struct(s)) -> StringMap.find s struct_decls
                   in 
-                  let index = find_field_num s.fields s2 in
+                  let index = get_field_idx s.fields s2 in
                   let field' = L.build_struct_gep struct_val index s2 builder in
                   L.build_load field' "tmp" builder
                 | Dot -> 
                   let (struct', st) = lookup s1 in
                   let s_def = match st with 
-                    A.Struct(s) -> StringMap.find s all_structs
+                    A.Struct(s) -> StringMap.find s struct_decls
                   in 
-                  let index = find_field_num s_def.fields s2 in
+                  let index = get_field_idx s_def.fields s2 in
                   let field' = L.build_struct_gep struct' index s2 builder in
                   L.build_load field' "tmp" builder)
           | SDeref s -> 
@@ -402,4 +365,4 @@ let translate (globals, structs, functions) =
     exit_scope ();
   in
 List.iter build_function_body functions; 
-the_module
+crusty_module
