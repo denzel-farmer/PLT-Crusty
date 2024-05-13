c07ec3afd92e6595110c939a9abb9cc7465c11ea
Denzel Farmer <denzelgfarmer@gmail.com>
2024-05-12 23:29:27 -0400

    Modify checksemant

--- src/checksemant.ml
+++ src/checksemant.ml
@@ -3,12 +3,13 @@ let _ =
   let program = Crustyparse.program Scanner.token lexbuf in
   let sprogram = Semant.check program in
   let linear_result = Linear.check sprogram in
-  print_endline ("\nAbstract Syntax Tree:");
+  print_endline "\nAbstract Syntax Tree:";
   print_endline (Astprint.string_of_program program);
-  print_endline ("\nSemantic Checked Abstract Syntax Tree:");
+  print_endline "\nSemantic Checked Abstract Syntax Tree:";
   print_endline (Sastprint.string_of_sprogram sprogram);
-  print_endline ("\nLinearity Results:");
+  print_endline "\nLinearity Results:";
   print_endline (Linear.string_of_linear_result linear_result);
-  print_endline ("\nCOMPILATION SUCCESS");
-  print_endline ("\nScanned tokens:");
-;;
+  if Linear.lin_result_failed linear_result then
+    exit 1
+  else
+    print_endline ("\nCOMPILATION SUCCESS");
